0209cc256714721de73a639b85cd4a4ef32ebb3b
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-10-17 20:16:36 -0700

    task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 /**
- * main -  prints all the numbers of base 16 in lowercase
+ * main - prints all the numbers of base 16 in lowercase
  *
  * Return: 0 (seccess)
  */
 int main(void)
 {
 	char c;
-	for (c = '0'; c <= '9'; c++)
-	{
-		Putchar(c);
-	}
-	for (c = 'a'; c <='f'; c++)
-	{
-		putchar(c);
-	}
+	int d;
+	
+	c = 'a';
+	d = 0;
+	while 
+		(d < 10)
+		 {
+		  Putchar(d + '0');
+		  d++;
+		 }
+	while
+		(c <= 'f')
+		{
+			putchar(c);
+			c++;
+		}
 		putchar('\n');
 		rerurn (0);
-	}
+}
