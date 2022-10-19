e44d03770e36c788d8a7f85248fb51e8f85ccfd0
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-10-19 14:44:07 -0700

    done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
 #include "main.h"
-#include <man isalpha>
+
 /**
- * _isalpha - checks for alphabetic char
+ * _isalpha - checks for alphabets
  * @c: char to be checked
  *
- * Return: 1 if char is a letter,othrwise 0
+ * Return: 1 if char is a letter, othrwise 0
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && <= 'z') || (c >= 'A' && <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a numb
+ * @n: num to check
+ * Return: to the value of the last digit
+ */
+int print_last_digit(int)
+{
+	if ()
+		_putchar
+
