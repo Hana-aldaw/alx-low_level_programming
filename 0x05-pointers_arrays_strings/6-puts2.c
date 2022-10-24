a85553f67c91d63f3d4294196de928319e0e2358
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-10-24 16:28:58 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ *
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	len = 0;
+	
+	while (str[i++]
+			len++;
+			for (i = 0; i < len; i += 2)
+			_putchar(str[i];
+				_putchar('\n');
+}
+
