daedaa455c916f280e8260cb9fe0be14f9d2914e
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-10-24 14:20:34 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: string pointer
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
