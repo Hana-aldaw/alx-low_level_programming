9b8dc9e0de041fd2e9a8a9643a2230c88ab69658
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-10-24 16:55:59 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints half of a string
+ *
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i, len;
+
+	while (str[i++])
+		len++;
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
