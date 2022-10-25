f40ddc8896ed6a8ab1a41f503bf771dc8204c0f7
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-10-24 18:08:54 -0700

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,8 @@
 #include "main.h"
-
+#include <string.h>
 /**
- * _strcpy - copies the string pointed to by src
+ * _strcpy - copies the string pointed to by sorce
+ *
  * @dest: destination
  * @src: source
  * Return: string
@@ -9,11 +10,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int len = 0;
-	while (*(str + len) != '\0')
+
+	while (*(src + len) != '\0')
 	{
-		*(dest + len) = *(str + len);
+		*(dest + len) = *(src + len);
 		len++;
 	}
 	*(dest + len) = '\0';
+
 	return (dest);
 }
