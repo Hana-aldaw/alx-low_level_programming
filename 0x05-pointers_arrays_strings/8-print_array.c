d88b9e457d43c1c7039a471debf2bcbfe3771f3e
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-10-24 17:42:01 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ *
+ * @n: number of values
+ * @a: array
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		_putchar("%d", a[x]);
+		if (x != n - 1)
+			_putchar(", ");
+	}
+	_putchar('\n');
+}
