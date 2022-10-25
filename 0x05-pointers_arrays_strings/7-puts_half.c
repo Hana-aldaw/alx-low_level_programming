854da676e3c677fdbba72508e85d6ee92fe999f2
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-10-24 17:29:51 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,15 +4,15 @@
 /**
  * puts_half - function prints half of a string
  *
- * @str: string
+ * @str: string 
  */
 void puts_half(char *str)
 {
-	int i, x, y
+	int i, x, y;
 
 		x = strlen(str);
 	if (x % 2 == 1)
-	       y = x / 2 + 1;
+	       (y = x / 2 + 1);
 	else
 		y = x / 2;
 	for (i = y; i < x; i++)
