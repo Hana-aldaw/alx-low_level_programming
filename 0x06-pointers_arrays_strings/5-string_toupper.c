4ea56cec0abb61ac498d56e3ea5e73a3c26f2260
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-10-27 05:50:31 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ *
+ * @a: string param
+ * Return: string
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	    while (a[i])
+	    {
+		    if (a[i] >= 97 && x[i] <= 122)
+			    a[i] = a[i] - 32;
+		    i++;
+	    }
+	    return (a);
+}
