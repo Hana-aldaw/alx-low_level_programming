a488b241b1ec89abc3b8882be533ef9757e1d11d
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-10-31 14:42:36 -0700

    7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,20 +9,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int x;
-	int sum1 = 0;
-	int sum2 = 0;
+	int x, sum1 = 0, sum2 = 0;
 
-	for (x = 0; x < size * size; x++)
+	for (x = 0; x < size; x++)
 	{
-		if (x % (size + 1) == 0)
-		{
-			sum1 + a[x];
-		}
-		if (x % (size - 1) == 0 && (x > 0 && (x < (size * size - 1))))
-		{
-			sum2 += a[x];
-		}
+		sum1 += a[(size + 1) * x];
+		sum2 += a[(size - 1) * (x + 1)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
