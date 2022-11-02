a75c12f5b7093e5d4568db869e5eacb01c8321fa
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-11-02 16:05:21 -0700

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,9 +4,22 @@
  * _strlen_recursion - returns the length of a string
  * @s: string
  *
+ * Return: length of a string
  */
 
 int _strlen_recursion(char *s)
 {
+	int len = 0;
+
+	if (*s == '\0')
+	return (0);
+
+	if (*s != '\0')
+	{
+		len++
+			len += _strlen_recursion(s++);
+		return (len);
+	}
+	return (0);
 }
 
