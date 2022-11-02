70f79064e8008ffe59dbdeef504f72ea0499e79d
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-11-02 15:07:32 -0700

    done

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ * @s:
+ *
+ */
+
+int is_palindrome(char *s)
+{
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * is_prime_number -  returns 1 if the input integer is a prime number, otherwise return 0
+ *
+ */
+
+int is_prime_number(int n)
+{
+}
