2ec7dfd097006d468e713c83b0113398dff30638
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-11-02 16:26:43 -0700

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,9 +15,12 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 		return (-1);
 
-	else if (y == 0)
+	if (y == 0)
 		return (1);
 
-	result *= _pow_recursion(x, y - 1);
-	return (result);
+	else if (y % 2 == 0)
+
+		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
+	else
+		return (x *  _pow_recursion(x, y / 2) *  _pow_recursion(x, y / 2));
 }
