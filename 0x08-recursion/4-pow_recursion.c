ba2b0c5b1fd4671e936874be1dd5e4c1ca8014a3
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-11-02 16:18:25 -0700

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,11 +2,22 @@
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
- * @x:
- * @y:
+ * @x: thr raised number
+ * @y: the power
  *
+ * Return: the value of x raised to the power of y
  */
 
 int _pow_recursion(int x, int y)
 {
+	int result;
+
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (1);
+
+	result *= _pow_recursion(x, y - 1);
+	return (result);
 }
