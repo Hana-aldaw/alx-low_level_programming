b17c7dfe1c919785d3e537abce72c262cfe5cb2a
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-11-02 15:20:01 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,10 +2,14 @@
 
 /**
  * is_palindrome - returns 1 if a string is a palindrome and 0 if not
- * @s:
+ * @s: string
  *
+ * Return: 1 if palindrome and 0 if not
  */
 
 int is_palindrome(char *s)
 {
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
