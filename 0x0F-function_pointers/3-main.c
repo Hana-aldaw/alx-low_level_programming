67462a8e50341066840a404da10d0c0317696848
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-11-16 15:27:27 -0800

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,9 +11,8 @@
 
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char i;
-	int (*func)(int, int);
+	int a, b;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
@@ -21,24 +20,21 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
-	func = get_op_func(argv[2]);
-
-	if (!func)
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	i = *argv[2];
-	if ((i == '/' || i == '%') && arg2 == 0)
-			{
-				printf("Error\n");
-				exit(100);
-			}
-	result = func(arg1, arg2);
-	 printf("%d\n", result);
-	 return (0)
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%d\n", operation(a, b));
+	return (0);
 }
