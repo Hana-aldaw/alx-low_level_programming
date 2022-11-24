1414977ac59b51a4fdff3d15de775e4ea6f963d2
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-11-24 15:26:49 -0800

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,6 +9,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t x;
+
 	x = 0;
 
 	while (h != NULL)
@@ -16,7 +17,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
-		printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 	h = h->next;
 x++;
 	}
