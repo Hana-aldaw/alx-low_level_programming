589b4e64247c1b5ff7f16c631f9692f9bdc5600a
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-11-28 05:44:28 -0800

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,12 +6,15 @@
  * @s: string
  * @b: charcter
  * @n: integer
+ *
  * Return: string
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
+
 	for (a = 0; a < n; a++)
 		s[a] = b;
+
 	return (s);
 }
