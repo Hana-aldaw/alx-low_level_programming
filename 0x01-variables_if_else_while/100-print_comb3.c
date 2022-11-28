30b382d8b9f57480239d534e560736b0866ef243
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-11-27 16:51:08 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,23 +7,24 @@
  */
 int main(void)
 {
-	int ones = '0';
+	int n;
+	int i;
 
-	int tens = '0';
-
-	for (tens = '0'; tens <= '9'; tens++)
+	for (n = 0; n < 9; n++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
-		{
-			if (!((ones == tens) || (tens > ones)))
-			{
-				putchar(tens);
-				putchar(ones);
+		i = n + 1;
 
-				if (!(ones == '9' && tens == '8'))
-					putchar(',');
+		do {
+			putchar('0' + n);
+			putchar('0' + i);
+
+			if (n < 8)
+			{
+				putchar(',');
+				putchar(32);
 			}
-		}
+			i++;
+		} while (i < 10);
 	}
 	putchar('\n');
 	return (0);
