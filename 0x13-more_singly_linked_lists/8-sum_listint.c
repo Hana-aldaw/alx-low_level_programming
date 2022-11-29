668ebf03e298ea5b20c896d4ec37421918a394d7
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-11-29 13:56:05 -0800

    8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,3 +1,21 @@
+#include "lists.h"
 
+/**
+ * sum_listint - returns the sum of all the data (n) of a linkes list
+ * @head: head of a list
+ *
+ * Return: sum of all the data (n)
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
 
-:wq
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
