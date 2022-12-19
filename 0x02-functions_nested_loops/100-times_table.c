aa5611c6ba47d78dd7c2339e955fe08cd14abe07
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-12-19 08:57:24 -0800

    done

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table
+ * @n: the upper limit to be printed
+ */
+void print_times_table(int n)
+{
+	int row;
+	int column;
+	int product;
+
+	if (n < 0 || n >= 15)
+
