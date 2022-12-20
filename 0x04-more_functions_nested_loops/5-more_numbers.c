560f9d282c9d1f98d1afe2436e459b8a184feba4
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-12-20 02:40:48 -0800

    done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,17 @@
 
 void more_numbers(void)
 {
-	int x, y;
+	int x;
+	int y;
 
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
 			if (y >= 10)
-
-		_putchar('0' + (y / 10));
+			{
+				_putchar('0' + (y / 10));
+			}
 			_putchar('0' + (y % 10));
 		}
 		_putchar('\n');
