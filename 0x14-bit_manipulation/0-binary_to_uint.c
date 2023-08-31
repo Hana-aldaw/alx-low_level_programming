1fa231c99b2db7e468779077c2b2c4d5651b6dae
Hana-aldaw <hana.m.aldaw@gmail.com>
2023-08-31 15:16:06 -0700

    ok

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,24 +11,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int ui = 0;
+	int i;
 
 	if (!b)
 		return (0);
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (b[i] < '' || b[i] > '1')
 			return (0);
-		}
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
-		return (ui);
+		ui = 2 * ui + (b[i] - '0');
 	}
+	return (ui);
+}
