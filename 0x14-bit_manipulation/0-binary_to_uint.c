341db8c5625485e2285475e84417e8c021a2c406
Hana-aldaw <hana.m.aldaw@gmail.com>
2023-09-20 05:21:48 -0700

    1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,6 @@
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int
- *
  * @b: pointing to a string of 0 and 1 chars
  *
  * Return: the converted number, or 0 if
@@ -12,15 +11,25 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
-	int len, base
+	int len, base_two;
 
 	if (!b)
 		return (0);
-	for (i = 0; b[i]; i++)
+	ui = 0;
+	
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[i] < '' || b[i] > '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		ui = 2 * ui + (b[i] - '0');
+		}
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
 	return (ui);
 }
